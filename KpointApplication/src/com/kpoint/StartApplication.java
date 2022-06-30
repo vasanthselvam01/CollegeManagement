37ea9013d0768c57d79ade02181ef83ae457fde8
vasanthselvam vasanthselvam01@gmail.com <vasanthselvam vasanthselvam01@gmail.com>
2022-06-30 19:15:52 +0530

    commit 2

--- KpointApplication/src/com/kpoint/StartApplication.java
+++ KpointApplication/src/com/kpoint/StartApplication.java
@@ -3,7 +3,7 @@ package com.kpoint;
 public class StartApplication {
 	
 	public static void main(String[] arg) {
-		System.out.println("Start the project");
+		System.out.println("Start the project1");
 		System.out.println("Start the project2");
 		
 		
