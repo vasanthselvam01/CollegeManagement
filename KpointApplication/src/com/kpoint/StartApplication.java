e3d50f96c424edade3d4b97b3c73d0fce5d1fb92
vasanthselvam vasanthselvam01@gmail.com <vasanthselvam vasanthselvam01@gmail.com>
2022-06-30 16:56:52 +0530

    This is test commit

--- KpointApplication/src/com/kpoint/StartApplication.java
+++ KpointApplication/src/com/kpoint/StartApplication.java
@@ -5,6 +5,8 @@ public class StartApplication {
 	public static void main(String[] arg) {
 		System.out.println("Start the project");
 		System.out.println("Started the project and deployed");
+		
+		System.out.println("New statement is added");
 	}
 
 }
