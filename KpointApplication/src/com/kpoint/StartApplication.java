8c7fd2e31d7b8bf8f215d92968ee551439a2a560
vasanthselvam vasanthselvam01@gmail.com <vasanthselvam vasanthselvam01@gmail.com>
2022-06-30 18:21:46 +0530

    remove statements

--- KpointApplication/src/com/kpoint/StartApplication.java
+++ KpointApplication/src/com/kpoint/StartApplication.java
@@ -6,9 +6,6 @@ public class StartApplication {
 		System.out.println("Start the project");
 		System.out.println("Started the project and deployed");
 		
-		System.out.println("New statement is added");
-		System.out.println("New changes");
-		System.out.println("New changes1");
 	}
 
 }
