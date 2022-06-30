75e4592bd66aee862a7d21f9c73547e10deaba3b
vasanthselvam vasanthselvam01@gmail.com <vasanthselvam vasanthselvam01@gmail.com>
2022-06-30 16:51:45 +0530

    One more sysout is added on it

--- KpointApplication/src/com/kpoint/StartApplication.java
+++ KpointApplication/src/com/kpoint/StartApplication.java
@@ -4,6 +4,7 @@ public class StartApplication {
 	
 	public static void main(String[] arg) {
 		System.out.println("Start the project");
+		System.out.println("Started the project and deployed");
 	}
 
 }
